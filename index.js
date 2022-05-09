07eeccdf9531ea75af2165558c23baaaa5251024
Sunshine Abbott <sunshine.abbott@gmail.com>
2022-05-09 12:27:36 +0100

    video plays based on what button is clicked

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-let vid = document.getElementById("video"); 
 
-
-for (let i = 1; i < 4; i++) {
-    idNum += video[i];
+/* Sets the video based on the click of a button from the user. The initial video is set, but it gets replaced based on a click of a button. */
+function setvideo(src) {
+    document.getElementById('video-left').innerHTML = '<video id="video" controls><source src="'+src+'" type="video/mp4">Your browser does not support the video tag.</video>'; 
+    document.getElementById('video').play();
 }
 
-function playVid() { 
-  vid.play(); 
-} 
+/* TODO: set an event listener to determine when the video is done playing, and reset it.  */
+
+
+
 
