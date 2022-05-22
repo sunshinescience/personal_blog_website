e564dffacc32457f656231622a1b81f1e0452a1a
Sunshine Abbott <sunshine.abbott@gmail.com>
2022-05-22 13:40:46 +0100

    clicking through videos to the right

--- index.js
+++ index.js
@@ -1,57 +1,42 @@
 
 /* Sets the video based on the click of a button from the user. The initial video is set, but it gets replaced based on a click of a button. */
 
+/* 
+See if I can use anything found in here:
+https://developer.mozilla.org/en-US/docs/Web/Guide/Audio_and_video_delivery/Video_player_styling_basics
 
-function setvideo(src) {
-    document.getElementById('video-left').innerHTML = '<video id="video" controls><source src="'+src+'" type="video/mp4">Your browser does not support the video tag.</video>'; 
+https://blog.shahednasser.com/how-to-style-a-video-player-and-create-a-custom-player/
+*/
+
+/* The function below dynamically updates the video based on the user's click */
+function setVideo(src) {
+    document.getElementById('video-player').innerHTML = '<video id="video" controls><source src="'+src+'" type="video/mp4">Your browser does not support the video tag.</video>'; 
     document.getElementById('video').play(); 
 }
 
 
-/*
-document.getElementById('button-1').addEventListener('click', function() {
-    document.getElementById('source_video').src = '../images/Jamming_1_4.m4v'; 
-    document.getElementById('video-left').innerHTML = '<video id="video" controls><source src="'+src+'" type="video/mp4">Your browser does not support the video tag.</video>'; 
-    document.getElementById('video').play();
-});
-*/
+let src1 = '../images/Jamming_1_4.m4v';
+let src2 = '../images/Jamming_2_1.m4v';
+let src3 = '../images/Jamming_3_1.m4v';
 
-function getSrc() {
-    let currentSrc = document.getElementById('source_video').src;
-    alert(currentSrc);
-}
-// getSrc();
+srcList = [src1, src2, src3];
+let activeIndex = 0;
 
+// We use only one event listener, and listen for any click in the area of the video player container
+function getId(event) { 
+    // alert(event.target.id);
 
-document.getElementById('button-1').addEventListener('click', 
-    function() {
-   /* alert('button 1 was clicked, so if they click right arrow play next video to right'); */
-   let currentSrc = document.getElementById('source_video').src;
-   
-});
+    // note that I need to have a loop in case someone clicks through the videos
+    if (event.target.id == 'arrow-right') {
+        activeIndex += 1;
+        if (activeIndex == srcList.length) {
+            activeIndex = 0;
+        }
+      }
+      setVideo(srcList[activeIndex]);
 
-
-
-/* TODO: set an event listener to determine when the video is done playing, and reset it.  */
-/* Or, make videos so they end in a nice way */
+      // Do the same for the left button, but use -1
+      // also, the amount of circle buttons underneath should be the same as the list, change circle buttons so that it works like this too
+  }
 
 
-/* TODO: */
-/* Set an event listener for which video was played and which arrow is clicked in order to scroll through the videos. */
-function showVideoRight() {
-    
-    // alert("right was clicked"); 
-    /* Get which video is currently showing */
-
-    /* Display the video to the right when clicked */
-    let currentSrc = document.getElementById('source_video').src;
-    alert(currentSrc);
-    // http://127.0.0.1:5502/images/Jamming_1_4.m4v
-} 
-
-function showVideoLeft() {
-    alert("Left was clicked."); 
-     /* Get which video is currently showing */
-
-     /* Display the video to the left when clicked */
-} 
