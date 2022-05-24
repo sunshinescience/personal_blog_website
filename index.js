ef86358f6760bbc6195e6859817cff1d9bf94c35
Sunshine Abbott <sunshine.abbott@gmail.com>
2022-05-24 05:26:03 +0100

    buttons on video container work

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 
-/* Sets the video based on the click of a button from the user. The initial video is set, but it gets replaced based on a click of a button. */
-
-/* 
-See if I can use anything found in here:
-https://developer.mozilla.org/en-US/docs/Web/Guide/Audio_and_video_delivery/Video_player_styling_basics
-
-https://blog.shahednasser.com/how-to-style-a-video-player-and-create-a-custom-player/
-*/
-
-/* The function below dynamically updates the video based on the user's click */
+/* Dynamically updates the video-player element based on the user's click and plays the video */
 function setVideo(src) {
     document.getElementById('video-player').innerHTML = '<video id="video" controls><source src="'+src+'" type="video/mp4">Your browser does not support the video tag.</video>'; 
     document.getElementById('video').play(); 
 }
-
 
 let src1 = '../images/Jamming_1_4.m4v';
 let src2 = '../images/Jamming_2_1.m4v';
@@ -23,22 +13,34 @@ srcList = [src1, src2, src3];
 let activeIndex = 0;
 
 // We use only one event listener, and listen for any click in the area of the video player container
+// This function sets the active index of the video source list (srcList) and then it calls the setVideo function above with the active video source to play
 function getVideo(event) { 
     if (event.target.id == 'arrow-right') {
         activeIndex += 1;
         if (activeIndex == srcList.length) {
             activeIndex = 0;
         }
-      }
+    }
 
     else if (event.target.id == 'arrow-left') {
         activeIndex -= 1;
         if (activeIndex < 0 ) {
            activeIndex = srcList.length - 1;
-       }
-       alert(activeIndex);
-      }  
-      setVideo(srcList[activeIndex]);
-  }
+        }
+    } 
+    
+    else if (event.target.id == 'button-1') {
+        activeIndex = 0;
+    }
 
+    else if (event.target.id == 'button-2') {
+        activeIndex = 1;
+    }
+
+    else if (event.target.id == 'button-3') {
+        activeIndex = 2;
+    }
+  
+    setVideo(srcList[activeIndex]); // calling a function with the active video source in order to play the video
+}
 
